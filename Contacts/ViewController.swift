85862c78935768c8010ed4bc3eba4dcf5c5996e0
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-05-06 21:32:44 +0800

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Александр on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
